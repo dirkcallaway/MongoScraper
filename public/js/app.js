183722debcfd5acd2a1fef45ab4807ebfb3e1253
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-04-01 23:04:45 -0600

    Notes are added to DB

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
 
-$(".delete-btn").click(function (event) {
-  event.preventDefault();
-  const id = $(this).attr("data");
-  $.ajax(`/remove/${id}`, {
-      type: "PUT"
-  }).then(function(){
-      location.reload();
-  })
-});
-
+//Save Article Button
 $("#article-box").on('click','button',function() {
   console.log("Article clicked.")
   const id = $(this).attr("data-id");
@@ -20,6 +11,7 @@ $("#article-box").on('click','button',function() {
   })
 });
 
+//Remove Saved Article Button
 $("#saved-articles").on('click','#delete-saved',function() {
   console.log("Delete Article")
   const id = $(this).attr("data-id");
@@ -27,6 +19,35 @@ $("#saved-articles").on('click','#delete-saved',function() {
       type: "PUT"
   }).then(function(){
       location.reload();
+  })
+});
+
+//Article Note Button
+$("#saved-articles").on('click','#article-notes',function() {
+  $('#note-modal').modal('toggle')
+  console.log("Open Notes")
+  const id = $(this).attr("data-id");
+  $("#save-note").attr("data-id", id);
+  $.ajax(`/notes/${id}`, {
+      type: "GET"
+  }).then(function(){
+      location.reload();
+  })
+});
+
+//Save Note Button
+$(".modal-footer").on('click','#save-note',function() {
+  console.log("Save Notes")
+  const id = $(this).attr("data-id");
+  let note = {
+    noteBody: $("#note-body").val()
+  }
+  $.ajax(`/notes/${id}`, {
+      type: "POST",
+      data: note
+  }).then(function(){
+      // location.reload();
+      $("#note-body").val("")
   })
 });
 
--- server.js
+++ server.js
@@ -119,6 +119,21 @@ app.put("/remove/:id", function(req, res){
   })
 })
 
+//Remove a Saved a article
+app.get("/notes/:id", function(req, res){
+  const id = req.params.id;
+  db.Note.find({
+    _id: id
+  })
+  .then(function(){
+    console.log("Notes Retrieved!")
+    //res.reload(); Is this needed?
+  })
+  .catch(function(err){
+    res.json(err)
+  })
+})
+
 //Saved Articles
 app.get("/saved", (req, res) => {
   db.Article.find({})
@@ -132,6 +147,30 @@ app.get("/saved", (req, res) => {
   .catch(function(err){
     res.json(err);
   })
+});
+
+//Save Note
+// Route for saving/updating an Article's associated Note
+app.post("/notes/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  console.log(`The req is: ${req.body.noteBody}`)
+  db.Note.create({
+    body: req.body.noteBody
+  })
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
 // Listen on port 3000
